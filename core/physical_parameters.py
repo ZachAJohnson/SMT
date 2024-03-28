Corrected xc formula.

core/physical_parameters.py
@@ -27,10 +27,10 @@ def Electron_Plasma_Frequency(ne):
     ωe = np.sqrt(4 * π * ne /m_e)
     return ωe
 
-def Gamma(T, n, Z):
+def Gamma(T, n, Q): # Q is Zbar for ions
     β = 1/T
     rs = rs_from_n(n)
-    return Z**2*β/rs
+    return Q**2*β/rs
 
 def Debye_length(T, ni, Zbar):
     ne = Zbar*ni
@@ -125,12 +125,15 @@ def xc_YOT(Te, ne):
     β = 1/Te
     sech = lambda x: 1/np.cosh(x)
     γx = 1/(2*π) * (3*ne/(8*π))**(1/3) * np.tanh(4/(9*θ)) + ne/(12*Te) * sech(4/(9*θ))**2
-    a = -1.51
-    b = -0.840
-    c = 0.275
-    d = -0.553
 
-    γc = 1/4 * (β*ne/π)**(1/2) *( np.tanh(c*β**a*ne**b/(1+β**d)) + 2/3 * b*c*β**a/(1 + β**d*ne**(b+1/2))*sech(c*β**a*ne**b/(1+β**d))**2  )
+    c1 = -1.51
+    c2 = -0.840
+    c3 = 0.275
+    c4 = -0.553
+    c5 = 0.0103
+
+    γc = ( 1/4 * (β*ne/π)**(1/2) *( np.tanh(c3*β**c1*ne**c2/(1+β**c4)) + 2/3 * c2*c3*β**c1*ne**(c2 + 1/2)/(1 + β**c4)*sech(c3*β**c1*ne**c2/(1+β**c4))**2  ) 
+        - c5*( np.log(1 + ne) + ne/(1+ne) )  )
     γxc = γx + γc
     return γxc
 

core/transport.py
@@ -63,7 +63,7 @@ class TransportProperties():
 	"""
 
 	def __init__(self, N_ions, ion_masses_AU, Zion_array, T_array_AU, ni_array_AU, Zbar_type='TF', Zbar_array=None,
-						 improved_xc_SMT=False, improved_λdB_SMT=False, improved_ae_SMT=False, improved_PauliBlocking=False,  xc_type='PDW', λdB_n = 2):
+						 improved_xc_SMT=False, improved_λdB_SMT=False, improved_ae_SMT=False, improved_PauliBlocking=False,  xc_type='YOT', λdB_n = 2):
 		"""
 		Defaults to SMT model in [1,2], with improved_... denoting the improvements in [3]
 		"""
@@ -455,10 +455,10 @@ class TransportProperties():
 		Γ_array = self.Zbar_array**2/(ai_array*self.Ti_array)
 
 		# Yukawa melting transition temperature
-		Γ_melt_array = 171.8 + 82.8*(np.exp(0.565*κ_array**1.38) - 1) 
+		self.Γ_melt_array = 171.8 + 82.8*(np.exp(0.565*κ_array**1.38) - 1) 
 
 		# Actual YVM formula
-		self.η_YVM_array = η_0_array*(0.0051*Γ_melt_array/Γ_array + 0.374*Γ_array/Γ_melt_array + 0.022)
+		self.η_YVM_array = η_0_array*(0.0051*self.Γ_melt_array/Γ_array + 0.374*Γ_array/self.Γ_melt_array + 0.022)
 		return self.η_YVM_array # * AU_to_g*AU_to_invcc*AU_to_cm**2/AU_to_s
 
 	def IYVM_viscosity(self):
@@ -478,7 +478,7 @@ class TransportProperties():
 		Γ_array = self.Zbar_array**2/(ai_array*self.Ti_array)
 
 		# Yukawa melting transition temperature
-		Γ_melt_array = 171.8 + 82.8*(np.exp(0.565*κ_array**1.38) - 1) 
+		self.Γ_melt_array = 171.8 + 82.8*(np.exp(0.565*κ_array**1.38) - 1) 
 
 		# Now for numerical parameters for fit
 		A = 1.45e-4 - 1.04e-4*κ_array + 3.69e-5*κ_array**2
@@ -488,6 +488,6 @@ class TransportProperties():
 		b = 1.63 - 0.325*κ_array + 0.24*κ_array**2
 
 		# Actual IYVM formula
-		self.η_IYVM_array = η_0_array*( A*(Γ_melt_array/Γ_array)**a + B*(Γ_array/Γ_melt_array)**b + C )
+		self.η_IYVM_array = η_0_array*( A*(self.Γ_melt_array/Γ_array)**a + B*(Γ_array/self.Γ_melt_array)**b + C )
 
 		return self.η_IYVM_array # * AU_to_g*AU_to_invcc*AU_to_cm**2/AU_to_s